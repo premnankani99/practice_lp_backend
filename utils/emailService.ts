305ff45dc80d4befe5379578567faa056b600c07
premnankani99 <premnankani99@gmail.com>
2026-08-03 12:40:26 +0530

    Fix ENETUNREACH IPv6 issue for Gmail on Render

--- utils/emailService.ts
+++ utils/emailService.ts
@@ -12,6 +12,7 @@ const transporter = nodemailer.createTransport({
         user: process.env.SMTP_USER,
         pass: process.env.SMTP_PASS,
     },
+    family: 4
 });
 
 export interface SendEmailOptions {
